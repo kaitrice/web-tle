1397ddbc9d7b56f27708aa29d933f6b4bd5e4736
kaitrice <kaitnrice@gmail.com>
2025-05-05 19:53:00 -0700

    fix youtube link

--- app/_components/common/Socials.tsx
+++ app/_components/common/Socials.tsx
@@ -5,7 +5,7 @@ import { Social } from "@/app/_types/socials.type"
 const socials = [
     { url: "https://www.instagram.com/theluckyeffectt/", icon: "/svg/white/icons8-instagram.svg", alt: "Instagram icon", disabled: false },
     { url: "https://www.tiktok.com/@theluckyeffectt?lang=en", icon: "/svg/white/icons8-tiktok.svg", alt: "TikTok icon", disabled: false },
-    { url: "https://www.youtube.com/@TheLuckyEffect", icon: "/svg/white/icons8-youtube.svg", alt: "YouTube icon", disabled: false },
+    { url: "https://www.youtube.com/@TheLuckyEffect77", icon: "/svg/white/icons8-youtube.svg", alt: "YouTube icon", disabled: false },
     { url: "mailto:theluckyeffect77@gmail.com", icon: "/svg/white/mail-white.svg", alt: "Envelope icon", disabled: false },
     { url: "https://music.apple.com/us/artist/the-lucky-effect/1790610682", icon: "/svg/white/icons8-music.svg", alt: "Apple music icon", disabled: false },
     { url: "https://open.spotify.com/artist/5Tr5sJICcc4lN5ppznL5fR", icon: "/svg/white/icons8-spotify.svg", alt: "Spotify icon", disabled: false },
