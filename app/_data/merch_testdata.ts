cae7da2570089437a0ae12d779ea9e251418fa00
Kaitlyn Rice <kaitnrice@gmail.com>
2025-11-07 13:59:11 -0800

    update data

---
+++ app/_data/merch_testdata.ts
@@ -0,0 +1,18 @@
+export const merch = [
+    {
+        type: "unisex t-shirt",
+        color: "black",
+        price: 20.00,
+        currency: "USD",
+        img: "/img/products/tshirt_black.png",
+        url: "https://tr.ee/x4kc2eWj9H"
+    },
+    {
+        type: "unisex t-shirt",
+        color: "white",
+        price: 20.00,
+        currency: "USD",
+        img: "/img/products/tshirt_white.png",
+        url: "https://tr.ee/x4kc2eWj9H"
+    }
+]
