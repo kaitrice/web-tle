7ce9a9df8ab8a37c94c6d4393d186211dbc233ee
Kaitlyn Rice <kaitnrice@gmail.com>
2025-11-17 14:52:40 -0800

    implement api key

--- backend/src/functions/web-tle-api-v1.ts
+++ backend/src/functions/web-tle-api-v1.ts
@@ -3,15 +3,26 @@ import { normalize_data } from "../../normalize";
 import { Artist } from "../../types";
 
 export async function web_tle_api_v1(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const key = request.headers.get("x-api-key")
+
+    if (!key || key !== process.env.BUILD_API_KEY) {
+        context.log('UNAUTHORIZED REQUEST:', key, request.url);
+        return {
+            status: 401,
+            body: "Unauthorized request."
+        }
+    }
+
+
     let data: Artist;
 
     try {
         data = await normalize_data();
     } catch (err) {
-        context.log('Error fetching data:', err);
+        context.log('ERROR FETCHING DATA:', err);
         return {
             status: 500,
-            body: "Server error. Please try again."
+            body: "Server error. Please try again later."
         }
     }
     
@@ -21,8 +32,8 @@ export async function web_tle_api_v1(request: HttpRequest, context: InvocationCo
     };
 };
 
-app.http('web-tle-api-v1', {
-    methods: ['GET', 'POST'],
+app.http('data', {
+    methods: ['GET'],
     authLevel: 'anonymous',
     handler: web_tle_api_v1
 });
