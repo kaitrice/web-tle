d49d0844be743bf253d87233b28ec2524448b5bd
Kaitlyn Rice <kaitnrice@gmail.com>
2025-11-17 13:43:37 -0800

    update spelling

--- backend/music/spotify.ts
+++ backend/music/spotify.ts
@@ -1,7 +1,7 @@
 async function get_access_token() {
     try {
         var client_id = process.env.SPOTIFY_CLIENT;
-        var client_secret = process.env.SPOTFY_SECRET;
+        var client_secret = process.env.SPOTIFY_SECRET;
 
         const response = await fetch("https://accounts.spotify.com/api/token", {
             method: "POST",
