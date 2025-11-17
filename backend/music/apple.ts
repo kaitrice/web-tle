4bfab7a682c3bc70959882875d5574ba0f54ee41
Kaitlyn Rice <kaitnrice@gmail.com>
2025-11-16 18:54:53 -0800

    generalize func

--- backend/music/apple.ts
+++ backend/music/apple.ts
@@ -1,10 +1,10 @@
-export async function fetch_apple_music() {
+export async function fetch_apple_music({ entity }: { entity: string }) {
     try {
         var id = process.env.APPLE_ARTIST_ID;
-        const url = `https://itunes.apple.com/lookup?id=1790610682&entity=album`;
+        const url = `https://itunes.apple.com/lookup?id=${id}&entity=${entity}`;
 
         const response = await fetch(url);
-        return await response.json();        
+        return await response.json();
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
