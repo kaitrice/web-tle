83cb0e2bc1072ff82bd556163705a74f4b2fcb20
Kaitlyn Rice <kaitnrice@gmail.com>
2025-11-20 13:19:16 -0800

    allow external image sources

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.scdn.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.*',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'highwiredaze.b-cdn.net',
         pathname: '/**',
       },
     ],
