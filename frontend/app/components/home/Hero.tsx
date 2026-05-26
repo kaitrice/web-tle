52de24516c79ef0c64a3179c769336297f7fb0cb
Kaitlyn Rice <kaitnrice@gmail.com>
2026-05-26 14:13:29 -0700

    fix button centering on mobile

--- frontend/app/components/home/Hero.tsx
+++ frontend/app/components/home/Hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Hero() {
 	return (
 		<section className="hero-bg flex justify-center items-center text-white">
-			<div className="border-12 flex justify-center items-center 2xl:items-end 2xl:pb-24 w-5/6 h-2/3">
+			<div className="border-12 flex text-center justify-center items-center 2xl:items-end 2xl:pb-24 w-5/6 h-2/3">
 				<Link href='/#music' className="max-w-2/3">
 					<p className="btn text-xl px-6 py-4">
 						Stream Our New Music
